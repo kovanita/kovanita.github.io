15eebe8f5586189373c061abb62655ca9e9f8a54
kovanita <63338755+kovanita@users.noreply.github.com>
2020-04-12 15:02:33 +0200

    practicing

--- Exercises/Directions/dist/script.js
+++ Exercises/Directions/dist/script.js
@@ -6,11 +6,32 @@ let printNumbersTill = (a) => {
 }
 
 
+
+
+
 printNumbersTill(20);
-
-
 let greetingTo = (name) => {
   return ('hello' + name);
 }
 greetingTo('Mark');
 console.log(greetingTo('Mark'))
+
+
+
+
+let printValues = (arr) => {
+  for(let i=0; i<arr.length; i++) {
+    console.log(arr[i])
+  }
+}
+printValues([0, 3, 6, 7, 9])
+
+
+
+
+
+let myNumbers = [10, 20, 30, 50, 12];
+myNumbers.forEach((number) => {
+  console.log(number);
+});
+
