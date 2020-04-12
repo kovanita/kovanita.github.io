25cbd92303e3c1bd795d37e9d81dd805644e5bcb
kovanita <63338755+kovanita@users.noreply.github.com>
2020-04-12 12:17:04 +0200

    loop exercise2

--- Exercises/Directions/dist/script.js
+++ Exercises/Directions/dist/script.js
@@ -1,4 +1,4 @@
-let numbers = 10;
+let numbers = 20;
 
-for (let i = 0; i < numbers; i++) 
-  if (i<numbers)  { console.log( i)}
+for (let i = 0; i <= numbers; i++) 
+  if (i%2===0)  {console.log(i)}
