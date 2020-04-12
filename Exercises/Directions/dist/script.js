7585d7b38713198c60ddac012adae62b2803b768
kovanita <63338755+kovanita@users.noreply.github.com>
2020-04-12 12:02:07 +0200

    if exercises

--- Exercises/Directions/dist/script.js
+++ Exercises/Directions/dist/script.js
@@ -1,4 +1,6 @@
-let myVariable = true;
-if (myVariable ===true) {
-  console.log('The condition has been evaluated to true');
+let a = 7;
+if (a < 5) {
+  console.log(a / 2);
+} else {
+  console.log(a / 7);
 }
