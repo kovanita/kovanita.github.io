0909ef689d1faeb0ff7f481ca37331f898c2637e
kovanita <63338755+kovanita@users.noreply.github.com>
2020-04-13 11:14:21 +0200

    Intro to jQuery

--- Exercises/Directions/dist/script.js
+++ Exercises/Directions/dist/script.js
@@ -34,3 +34,33 @@ myNumbers.forEach((number) => {
   console.log(number);
 });
 
+
+
+
+
+let col= 'purple';
+let num= 10;
+let wor= "cool";
+let backgroundColor = $('.elso').css('background');
+let sectext= $('.masodik');
+sectext=100;
+let thirtext = $('.harmadik') .text ('South');
+
+
+if ( backgroundColor === col ) {
+  $('.elso').css('background', 'purple');
+}else{
+  $('.elso').css('background','red')
+}
+
+if ( num > sectext ) {
+  $ ( '.masodik' ) .text ( "whoah, that's a big number." )
+} else {
+  $ ('.masodik') .text ( "just a regular number, please.")
+}
+
+if ( thirtext === wor ) {
+  $( '.harmadik' ) .text ( "Power of DOM")
+} else {
+  $( '.negyedik' ) .text ( "Power of DOM")
+}
