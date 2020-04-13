a36a79caa0305c18c035a6c8356da879183ab794
kovanita <63338755+kovanita@users.noreply.github.com>
2020-04-13 15:26:35 +0200

    events

--- Exercises/new webpage/script.js
+++ Exercises/new webpage/script.js
@@ -7,7 +7,13 @@ $('.firbut').click((myFunction) => {
 $('.secbut').click((functy) => {
     $('.firbut').text("it changed");
 });
+let firstClick = true
 
 $('.thibut').click((functy) => {
-    $('button').click($('button').css('background-color', 'blue'))
+    if (firstClick == true) {
+        let szinErtek = $('input').val();
+        $('button').click($('button').css('background-color', szinErtek))
+        firstClick = false 
+    }
 });
+
