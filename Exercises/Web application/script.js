77a06755d3a848f6ae4868f1c203e9325484bec3
kovanita <63338755+kovanita@users.noreply.github.com>
2020-04-15 10:10:57 +0200

    webAplThirdDay

---
+++ Exercises/Web application/script.js
@@ -0,0 +1,50 @@
+let imgData0={
+    photo: ".\\image gallery\\fullhd-hatterkep-hosszu-ut-16703090.jpg",
+    title: 'Út',
+    description: 'Hosszú út az erdő között.'
+}
+$('#kozeokep').attr('.\\image gallery\\fullhd-hatterkep-hosszu-ut-16703090.jpg', imgData0.photo);
+
+let imgData1={
+    photo: ".\\image gallery\\20160831_191037.jpg",
+    title: 'Naplemente',
+    description: 'Naplemente a Duna esztergomi partján.'
+}
+$('kozepkep').attr('.\\image gallery\\20160831_191037.jpg', imgData1.photo);
+
+let imgData2={
+     photo:'.\\image gallery\\20161230_132042.jpg',
+     title: 'Havas fenyők.',
+     description: 'Tél van a Hargitaán.'
+}
+$('kozepkep').attr('.\\image gallery\\20161230_132042.jpg', imgData2.photo);
+
+let imgData3={
+    photo:'.\\image gallery\\1448913197194.jpg',
+    title: 'Festett láda',
+    description:'Díjnyertes festett láda Nagyszebenben.'
+}
+$('kozepkep').attr('.\\image gallery\\1448913197194.jpg', imgData3.photo);
+
+let imgData4={
+    photo:'.\\image gallery\\DSC_0010.JPG',
+    title:'Vár',
+    description:'Vajdahunyad vára.'
+}
+$('kozepkep').attr('.\\image gallery\\DSC_0010.JPG', imgData4.photo);
+
+let imgData5={
+    photo:'.\\image gallery\\image-0-02-05-164d45a258ce5cfcf85a46754cb826042c7c1fa719f749e0397a17c4faebc226-V.jpg',
+    title:'Éjszakai repülés',
+    description:'Repülőből nézve az éjszaka.'
+}
+$('kozepkep').attr('.\\image gallery\\image-0-02-05-164d45a258ce5cfcf85a46754cb826042c7c1fa719f749e0397a17c4faebc226-V.jpg', imgData5.photo);
+
+
+let imgFolder=[imgData0,imgData1,imgData2,imgData3,imgData4,imgData5];
+
+imgFolder.forEach((image) => {
+    let currentPhoto
+}
+
+)
